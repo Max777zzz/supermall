c6ef4b75b33881244ec8aa5867d096242fe32339
林嘉威 <7640824+MaxVeryVV@user.noreply.gitee.com>
2020-09-01 01:35:50 +0800

    第二次提交

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,15 +3,41 @@
     <nav-bar>
       <div slot="center">购物街</div>
     </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <home-recommend :recommends="recommends"></home-recommend>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childComponents/HomeSwiper'
+import HomeRecommend from './childComponents/HomeRecommend'
+import { getHomeMultidata } from 'network/home'
 export default {
   name: 'Home',
   components: {
     NavBar,
+    HomeSwiper,
+    HomeRecommend,
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    }
+  },
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then(
+      (res) => {
+        console.log(res)
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      },
+      (err) => {
+        console.log(err)
+      }
+    )
   },
 }
 </script>
---
+++ src/views/home/childComponents/HomeRecommend.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="recommend">
+    <div
+      class="recommend-item"
+      v-for="(item, index) in recommends"
+      :key="index"
+    >
+      <a :href="item.link">
+        <img :src="item.image" alt="" />
+        <div>{{ item.title }}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return []
+      },
+    },
+  },
+}
+</script>
+
+<style scoped>
+.recommend {
+  display: flex;
+  width: 100%;
+  text-align: center;
+  font-size: 12px;
+  padding: 10px 0 20px 0;
+  border-bottom: 10px #eee solid;
+}
+.recommend-item {
+  flex: 1;
+}
+.recommend-item a {
+  text-decoration: none;
+  color: #666;
+}
+.recommend-item img {
+  width: 68px;
+  height: 65px;
+  margin-bottom: 12px;
+}
+</style>
